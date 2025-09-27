a3d80fb771a2338bd776321fc59f531237149a33
Valeria Romero <valellope@gmail.com>
2025-09-27 01:20:41 -0600

    fix confirmacion

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 
-const form = document.getElementById('confirmacion');
-const status = document.getElementById('status');
+document.getElementById("confirmacion").addEventListener("submit", function (event) {
+    // event.preventDefault(); // 👈 evita que el form recargue la página
+    console.log("Click form")
+    let formData = new FormData(this);
+    console.log(formData)
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const data = Object.fromEntries(new FormData(form).entries());
-    console.log(data)
-    try {
-        const res = await fetch("https://script.google.com/macros/s/AKfycbzXWNUz-6HhNy_KxFkkviBKsQulbUvaqYwZoMJM_oyleSfedpd4TpTGGiR8ZfW6k1CT9w/exec", {
-            method: "POST",
-            // headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data)
+    fetch("https://script.google.com/macros/s/AKfycby1bjBaWf1xLj6RLdjv8mdufuXV6inx69y7cFkg4xZtgJBCTHeZuzuIT-Le6vzD11cOuw/exec", {
+        method: "POST",
+        body: formData
+    })
+        .then(res => res.json())
+        .then(data => {
+            if (data.result === "success") {
+                let msg = document.getElementById("status");
+                msg.style.display = "block";
+                msg.style.color = "green";
+                msg.innerText = "Gracias 🎉, tu formulario fue enviado.";
+            }
+        })
+        .catch(err => {
+            console.error(err);
         });
-        status.textContent = "Recibimos tu confirmación, muchas gracias!";
-        form.reset();
-    } catch (err) {
-        status.textContent = "❌ Error submitting form.";
-        console.error(err);
-    }
 });
+
